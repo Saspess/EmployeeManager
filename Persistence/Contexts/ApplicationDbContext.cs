81ec11b3385a0be9d1168c06aef07996fc16a71f
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-12-12 18:04:50 +0300

    Add database context

---
+++ Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces.Contexts;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Department> Departments { get; set; } = null!;
+        public DbSet<DepartmentAddress> DepartmentsAddresses { get; set; } = null!;
+        public DbSet<DepartmentDataset> DepartmentsDatasets { get; set; } = null!;
+        public DbSet<Education> Educations { get; set; } = null!;
+        public DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<EmployeeAddress> EmployeesAddresses { get; set; } = null!;
+        public DbSet<EmployeeDataset> EmployeesDatasets { get; set; } = null!;
+        public DbSet<Organization> Organizations { get; set; } = null!;
+        public DbSet<OrganizationAddress> OrganizationsAddresses { get; set; } = null!;
+        public DbSet<OrganizationContact> OrganizationsContacts { get; set; } = null!;
+        public DbSet<OrganizationDataset> OrganizationsDatasets { get; set; } = null!;
+        public DbSet<Position> Positions { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public async Task<int> SaveChangesAsync() => await base.SaveChangesAsync();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
+
