27d547ad50482b04da25721f290788188c873dec
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-12-12 20:12:04 +0300

    Add interface for database context

---
+++ Application/Interfaces/Contexts/IApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Application.Interfaces.Contexts
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Department> Departments { get; set; }
+        DbSet<DepartmentAddress> DepartmentsAddresses { get; set; }
+        DbSet<DepartmentDataset> DepartmentsDatasets { get; set; }
+        DbSet<Education> Educations { get; set; }
+        DbSet<Employee> Employees { get; set; }
+        DbSet<EmployeeAddress> EmployeesAddresses { get; set; }
+        DbSet<EmployeeDataset> EmployeesDatasets { get; set; }
+        DbSet<Organization> Organizations { get; set; }
+        DbSet<OrganizationAddress> OrganizationsAddresses { get; set; }
+        DbSet<OrganizationContact> OrganizationsContacts { get; set; }
+        DbSet<OrganizationDataset> OrganizationsDatasets { get; set; }
+        DbSet<Position> Positions { get; set; }
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
