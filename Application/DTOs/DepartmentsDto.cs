8196c6f4a8b7715c34beada9c3881a062f3e47f1
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-12-12 20:08:28 +0300

    Add DTOs

---
+++ Application/DTOs/DepartmentsDto.cs
@@ -0,0 +1,15 @@
+﻿using Application.ViewModels;
+
+namespace Application.DTOs
+{
+    public class DepartmentsDto
+    {
+        public string Name { get; set; }
+        public IEnumerable<DepartmentViewModel> Departments { get; set; }
+
+        public DepartmentsDto(IEnumerable<DepartmentViewModel> organizations)
+        {
+            Departments = organizations;
+        }
+    }
+}
---
+++ Application/DTOs/EmployeesDto.cs
@@ -0,0 +1,16 @@
+﻿using Application.ViewModels;
+
+namespace Application.DTOs
+{
+    public class EmployeesDto
+    {
+
+        public string Name { get; set; }
+        public IEnumerable<EmployeeViewModel> Employees { get; set; }
+
+        public EmployeesDto(IEnumerable<EmployeeViewModel> employees)
+        {
+            Employees = employees;
+        }
+    }
+}
