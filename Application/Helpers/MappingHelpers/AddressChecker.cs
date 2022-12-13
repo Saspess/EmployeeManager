7916fdce6962a2dc0ed3189a9e5eaabaa4d300f6
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-12-13 14:57:13 +0300

    Add mapping helpers

---
+++ Application/Helpers/MappingHelpers/AddressChecker.cs
@@ -0,0 +1,29 @@
+﻿namespace Application.Helpers.MappingHelpers
+{
+    public static class AddressChecker
+    {
+        public static string Check(string houseCode)
+        {
+            if(houseCode != null)
+            {
+                return $"/{houseCode}";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public static string Check(int? flat)
+        {
+            if (flat != null)
+            {
+                return $" {flat}";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
