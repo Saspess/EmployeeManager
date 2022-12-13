33656bbed69ea70bd188d6a27bfff3a2e4a6f1e2
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-12-13 14:58:02 +0300

    Add IoC for Application

---
+++ Application/IoC/ServicesExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces.ServiceManager;
+using Application.ServiceManagers;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application.IoC
+{
+    public static class ServicesExtensions
+    {
+        public static void ConfigureServiceManager(this IServiceCollection services) =>
+            services.AddScoped<IServiceManager, ServiceManager>();
+
+        public static void ConfigureFluentValidation(this IServiceCollection services) =>
+            services.AddFluentValidation(opt =>
+            {
+                opt.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            });
+    }
+}
