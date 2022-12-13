17394fd7af6341296d9f19b5ce46ff8949038869
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-12-13 14:46:56 +0300

    Add interfaces for repositories

---
+++ Application/Interfaces/Repositories/IEducationRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IEducationRepository : IGenericRepository<Education>
+    {
+        Task<Education?> GetByNameAsync(string institutionName);
+    }
+}
