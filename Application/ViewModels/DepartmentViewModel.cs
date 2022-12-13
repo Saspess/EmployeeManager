40b2804fd231fe3a7b2749c4d74faa64091cbf88
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2022-12-13 14:54:47 +0300

    Add view models

---
+++ Application/ViewModels/DepartmentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.ViewModels
+{
+    public class DepartmentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string OrganizationName { get; set; } = null!;
+        public string DirectorName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
---
+++ Application/ViewModels/OrganizationViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.ViewModels
+{
+    public class OrganizationViewModel
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = null!;
+        public string DirectorName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
